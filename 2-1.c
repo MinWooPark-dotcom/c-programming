f180a9fe1e90364ee39df0461dbd7848ab78fc83
Minwoo Park <positive1attraction@gmail.com>
2021-08-19 20:19:13 +0900

    rm

--- 2-1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(void) 
-{
-    printf("1단계: 코드 분석 2단계: 코드 작성 3단계: 코드 비교 \n");
-    return 0;
-}
--- 2-2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(void) 
-{
-    printf("Hello C world"); // \n: new line, 개행
-
-    return 0;
-}
